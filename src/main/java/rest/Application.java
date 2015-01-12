84f1739d8032e88f05bb0be202afa7a44da4c1c2
mgryszko <marcin@grysz.com>
2015-01-12 14:51:13 +0100

    Added HAL support

--- src/main/java/rest/Application.java
+++ src/main/java/rest/Application.java
@@ -4,14 +4,15 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 
 @Configuration
 @EnableAutoConfiguration
+//@EnableHypermediaSupport(type = { HypermediaType.HAL })
 @ComponentScan
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-} 
+}
